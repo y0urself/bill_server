a6cfe8a9fb952603711cd190881db77b1badc9e7
Jaspar Löchte <jaspar.loechte@greenbone.net>
2020-07-16 08:12:53 +0200

    ._.

--- billservd/billservd/settings.py
+++ billservd/billservd/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!el%mgfp0-cnsl#&$!_v(ou^x*jl-+h4-+4bi+r4d(t%yrt8o%'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bills.s3lf.net', '127.0.0.1']
 
 
 # Application definition
